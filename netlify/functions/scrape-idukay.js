a56bdf2cb600f4ab109f0614081d5eb73c2c1abb
Crip <emilio.sebastianc@gmail.com>
2024-12-01 21:23:51 -0500

    solved part 5000

--- netlify/functions/scrape-idukay.js
+++ netlify/functions/scrape-idukay.js
@@ -1,11 +1,13 @@
-import puppeteer from 'puppeteer';
+// netlify/functions/scrape-idukay.js
+import puppeteer from 'puppeteer-core';
 import fetch from 'node-fetch';
 const { UserAgent, random } = require('user-agents');
 const url = 'https://idukay.net/api/students?__sort={"relational_data.name.order":"asc"}&populate={"user":"photo name surname second_name second_surname"}&select=user';
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { username, password } = req.body;
+export async function handler(event, context) {
+  if (event.httpMethod === 'POST') {
+    const { username, password } = JSON.parse(event.body); // Obtener los datos desde el cuerpo de la solicitud
+    
     try {
       let notas = [];  // Variable para almacenar la respuesta
       let lujus = "";  // Variable para almacenar la respuesta
@@ -22,52 +24,34 @@ export default async function handler(req, res) {
       await page.setUserAgent(
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
       );
-      // Intercepta las respuestas para capturar el token
+      
       page.on('response', async (response) => {
         try {
-          // console.log('Interceptando respuesta de URL:', response.url());
-          
-          // Verifica si la URL contiene alguna parte relevante del endpoint
           if (response.url().includes(targetEndpoint)) {
-            console.log('Posible respuesta relevante encontrada.');
-            
-            // Intenta obtener el cuerpo de la respuesta
-            const responseBody = await response.text(); // Cambiar a text para inspeccionar todo
-            console.log('Cuerpo de la respuesta:', responseBody);
-            
-            // Intenta parsear como JSON si es posible
-            try {
-              const jsonResponse = JSON.parse(responseBody);
-              if (jsonResponse.response.token) {
-                authToken = jsonResponse.response.token;
-                studentID = jsonResponse.response.user.preferences.selected_student._id;
-                schooolID = jsonResponse.response.user.school;
-                lectiveID = jsonResponse.response.user.preferences.working_year._id;
-                profileID = jsonResponse.response.user.preferences.working_profile._id;
-                console.log('Token de autorización capturado:', authToken);
-              }
-            } catch (err) {
-              console.log('No es JSON válido:', err);
+            const responseBody = await response.text();
+            const jsonResponse = JSON.parse(responseBody);
+            if (jsonResponse.response.token) {
+              authToken = jsonResponse.response.token;
+              studentID = jsonResponse.response.user.preferences.selected_student._id;
+              schooolID = jsonResponse.response.user.school;
+              lectiveID = jsonResponse.response.user.preferences.working_year._id;
+              profileID = jsonResponse.response.user.preferences.working_profile._id;
             }
           }
         } catch (err) {
           console.error('Error al procesar la respuesta:', err);
         }
       });
+
       // Navega al sitio y realiza el inicio de sesión
       await page.goto('https://idukay.net/', { waitUntil: 'networkidle2' });
       await page.waitForSelector('input[type="password"]');
       await page.type('input[type="text"]', username);
       await page.type('input[type="password"]', password);
-      
-      await Promise.all([
-        page.waitForNavigation({ waitUntil: 'networkidle2' }),
-        page.click('button[type="submit"]'),
-      ]);
+      await Promise.all([page.waitForNavigation({ waitUntil: 'networkidle2' }), page.click('button[type="submit"]')]);
 
-      // await page.goto('https://idukay.net/#/my_reportcard')
-      // Espera a que se capture el token
       await delay(500);
+
       if (profileID != studentID) {
         const headers = {
           "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
@@ -78,10 +62,9 @@ export default async function handler(req, res) {
           "SelectedStudent": studentID,
           "ClientVersion": "7.0.0",
         };
-        console.log(headers);
+
         const response = await fetch(url, { method: "GET", headers });
-        lujus = await response.json(); // Almacena la respuesta en la variable
-        console.log("Datos almacenados:", lujus);
+        lujus = await response.json(); 
         for (const element of lujus.response) {
           try {
             studentID = element._id;
@@ -94,24 +77,15 @@ export default async function handler(req, res) {
               "SelectedStudent": studentID,
               "ClientVersion": "7.0.0",
             };
-        
-            console.log(headerse);
+
             const urll = `https://idukay.net/api/student_year_summary?include_averages=true&student=${studentID}`;
-            console.log(urll);
-        
-            const response = await fetch(urll, {
-              method: "GET",
-              headers: headerse, // Cambié "headerse" al nombre correcto: "headers"
-            });
-        
-            const resulto = await response.json(); // Almacena la respuesta en la variable
+            const response = await fetch(urll, { method: "GET", headers: headerse });
+            const resulto = await response.json();
             notas.push({ student: element.user.name + " " + element.user.surname, resulto });
-            console.log("Datos almacenados:", notas);
-        
           } catch (error) {
             console.error("Error en la solicitud:", error);
           }
-        }  
+        }
       } else {
         const headers = {
           "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
@@ -122,25 +96,31 @@ export default async function handler(req, res) {
           "SelectedStudent": studentID,
           "ClientVersion": "7.0.0",
         };
-        console.log(headers);
+
         const urll = `https://idukay.net/api/my_year_summary?include_averages=true`;
         const response = await fetch(urll, { method: "GET", headers });
-        notas = await response.json(); // Almacena la respuesta en la variable
-        console.log("Datos almacenados:", notas);
+        notas = await response.json();
       }
-      
-      // console.log(notas.response.subjects[1].courses[0].year_summary.terms[0].parts[0].activity_types[0].activities[2].score)
-      res.status(200).json({ success: true, notas });
-      console.log(notas.response);
-      
+
       await browser.close();
 
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ success: true, notas }),
+      };
+      
     } catch (err) {
       console.error('Error:', err);
-      res.status(500).json({ success: false, error: err.message });
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ success: false, error: err.message }),
+      };
     }
   } else {
-    res.status(405).json({ success: false, message: 'Método no permitido.' });
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ success: false, message: 'Método no permitido.' }),
+    };
   }
 }
 
