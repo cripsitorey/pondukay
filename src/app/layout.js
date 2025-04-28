dc9b99cb89e399d0912b20dbe0484d234eade570
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2025-04-28 10:04:11 -0500

    googleads

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import localFont from "next/font/local";
+import Script from "next/script";
 import "../styles/globals.css";
 
 const geistSans = localFont({
@@ -20,6 +21,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7479076094686196"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
